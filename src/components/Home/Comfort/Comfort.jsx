aa219bce18a5abf57b0af16bba6dc22c3ee6fe52
Obd2004 <obidjonabd1097@gmail.com>
2023-02-10 14:24:24 +0500

    add Comfort and Example pages

--- src/components/Home/Comfort/Comfort.jsx
+++ src/components/Home/Comfort/Comfort.jsx
@@ -1,9 +1,37 @@
+import Aos from 'aos'
 import React from 'react'
 import './Comfort.scss'
 
 function Comfort() {
+  Aos.init()
   return (
-    <div>Comfort</div>
+    <div className='comfort' id='comfort'>
+      <div className="comfort__container container">
+        <div className="comfort__container__title">
+          <h2>Удобный инструмент для онлайн образования</h2>
+        </div>
+        <div className="comfort__container__wrapper">
+          <div className="comfort__container__wrapper__left">
+            <p data-aos="fade-up">С помощью технологии вы можете легко записать <br /> свой видео-урок, снять онлайн-курс <br />
+              или провести вебинар.</p>
+            <p data-aos="fade-up">Система полностью автоматическая, позволяет в <br />
+              2 раза быстрее и в 3 раза экономичнее записать <br />  видео-урок, презентационный или рекламный <br /> видеоролик.</p>
+          </div>
+          <div className="comfort__container__wrapper__list">
+            <h3>1</h3>
+            <h3>2</h3>
+            <h3>3</h3>
+            <h4>4</h4>
+          </div>
+          <div className="comfort__container__wrapper__right">
+            <p data-aos="fade-up">Программно-аппаратный комплекс позволяет <br /> работать с дополненной реальностью, <br /> взаимодействовать с презентацией</p>
+
+
+            <p data-aos="fade-up">Наша компания имеет аккредитацию на всех <br /> популярных электронных площадках. Мы имеем опыт <br /> участия в гос. закупках и проведения их через <br /> электронную площадку.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Home/Examples/Examples.jsx
+++ src/components/Home/Examples/Examples.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import './Examples.scss'
 function Examples() {
   return (
-    <div>Examples</div>
+    <div className='examples'>
+      <div className="examples__container container">
+        <div className="examples__container__title">
+          <h2>Примеры видео из iDesk Studio</h2>
+        </div>
+        <div className="examples__container__videos">
+        <iframe data-aos="fade-right" width="362" height="208" src="https://www.youtube.com/embed/itk3W2wf7l0" title="Новая версия программы iDesk Studio (+ редактор презентаций)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe  data-aos="fade-top" width="362" height="207" src="https://www.youtube.com/embed/Q7CAh8Lk4is" title="Первая онлайн школа о жизни с близорукостью. Анастасия Никифорова." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe data-aos="fade-left" width="362" height="210" src="https://www.youtube.com/embed/MYEXt4nYOoI" title="Образовательные видеостудии iDesk Studio. Ваши уроки больше никогда не будут скучными!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
   )
 }
 
