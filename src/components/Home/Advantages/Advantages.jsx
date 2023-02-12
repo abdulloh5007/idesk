f253e1166416280e948ec40cfa3b716bb9843b85
Levou7 <levou070@gmail.com>
2023-02-12 14:34:14 +0500

    new

--- src/components/Home/Advantages/Advantages.jsx
+++ src/components/Home/Advantages/Advantages.jsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import './Advantages.scss'
 
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
-const loopArr = []
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 3,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  textAlign: 'center'
+};
+
+
 function Advantages() {
 
   const [scrol2, setScrol2] = React.useState(false)
   const offSet = 250;
-  const loop = 'ishladim'
 
   const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
   window.addEventListener('scroll', () => {
     if (getTop() >= offSet) {
       setScrol2(true)
-      if(!loopArr.includes(loop)){
-        loopArr.push(loop)
-      }
     } else {
       setScrol2(false)
     }
   })
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div className="bgBlack">
@@ -31,8 +50,23 @@ function Advantages() {
           <div className="advantage-cards">
             <div className="box card1">
               <div className={scrol2 ? 'animateImg' : ''}>
-                <img src="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/687289/retina_1708x683_op-Ten-Front-End-Design-Rules-For-Developers_Luke-Newsletter-d3a7d3e7430ee224cab75104f11342a0.png" alt="box-img" />
+                <img onClick={handleOpen} src="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/687289/retina_1708x683_op-Ten-Front-End-Design-Rules-For-Developers_Luke-Newsletter-d3a7d3e7430ee224cab75104f11342a0.png" alt="box-img" />
               </div>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography sx={{fontWeight: '600'}} id="modal-modal-title" variant="h4" component="h2">
+                    Оборудование
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2, fontWeight: '300' }}>
+                    Наша студия оснащена современной прозрачной доской с RGB LED подсветкой стекла и подсветкой спикера, видеокамерой с FullHD/4K качеством, звуковой системой, мониторами для спикера и оператора, светодиодными светильниками для освещения спикера и фонами (чёрный, бирюзовый, синий и хромакей).
+                  </Typography>
+                </Box>
+              </Modal>
               <div className={scrol2 ? 'animationBox' : 'box-img'}></div>
               <b>Профессиональное оборудование</b>
               <p>Качественные звук и картинка, профессиональное освещение, сенсорная прозрачная доска</p>
--- src/components/Home/Chat/Chat.jsx
+++ src/components/Home/Chat/Chat.jsx
@@ -1,13 +1,89 @@
-import React from 'react'
-import './Chat.scss'
+// import React from 'react'
+// import './Chat.scss'
+// import CommentIcon from '@mui/icons-material/Comment';
+// import { Button, Dropdown } from 'antd';
+
+// const items = [
+//   {
+//     key: '1',
+//     label: (
+//       <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+//         1st menu item
+//       </a>
+//     ),
+//   },
+//   {
+//     key: '2',
+//     label: (
+//       <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+//         2nd menu item
+//       </a>
+//     ),
+//   },
+//   {
+//     key: '3',
+//     label: (
+//       <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+//         3rd menu item
+//       </a>
+//     ),
+//   },
+// ];
+
+// function Chat() {
+//   return (
+//     <div className='chat'>
+//       <Dropdown
+//         menu={{
+//           items,
+//         }}
+//         placement="top"
+//         arrow
+//       >
+//         <Button><CommentIcon/></Button>
+//       </Dropdown>
+//     </div>
+//   )
+// }
+
+// export default Chat
+
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+
+const actions = [
+  { icon: <QuestionAnswerIcon />, name: 'Запись в тех поддержку' },
+  { icon: <FormatAlignLeftIcon />, name: 'Открытая линия' },
+];
 
 function Chat() {
-
   return (
-    <div className='chat'>
-        {/* <button></button>    */}
+    <div onMouseMove={()=> document.body.style.background = '#000'} className="chat" style={{background: '#2FB644'}}>
+      <Box sx={{background: '#2FB644', position: 'fixed', right: 0, bottom: '1%'}} >
+        <SpeedDial
+          ariaLabel="SpeedDial basic example"
+          sx={{ position: 'absolute', bottom: 16, right: 16 }}
+          icon={<SpeedDialIcon/>}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+              sx={{background: '#2FB644', color: 'white'}}
+              className='btn'
+            />
+          ))}
+        </SpeedDial>
+      </Box>
     </div>
-  )
+  );
 }
 
 export default Chat
